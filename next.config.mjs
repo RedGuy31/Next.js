3add50d78d42da5ffd33379897b844b955b69009
irakli maisuradze <irakli.maisuradze31.19@gmail.com>
2024-03-20 20:24:08 +0400

    update images

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: "localhost",
-        pathname: "**",
-        port: "3000",
-        protocol: "http",
-      },
-      {
-        pathname: "**",
-        protocol: "https",
-        hostname: "digitalhippo-production.up.railway.app",
-      },
-    ],
+    domains: ["localhost", "digitalhippo-production.up.railway.app"],
+    // remotePatterns: [
+    //   {
+    //     hostname: "localhost",
+    //     pathname: "**",
+    //     port: "3000",
+    //     protocol: "http",
+    //   },
+    //   {
+    //     pathname: "**",
+    //     protocol: "https",
+    //     hostname: "digitalhippo-production.up.railway.app",
+    //   },
+    // ],
   },
 };
 
