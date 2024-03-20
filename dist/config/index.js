f1f60ccc761fb6cf6448dec29f4abc3be7aa15e1
irakli maisuradze <irakli.maisuradze31.19@gmail.com>
2024-03-20 14:21:25 +0400

    first commit

---
+++ dist/config/index.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PORDUCT_CATEGORIES = void 0;
+exports.PORDUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui_kits",
+        featured: [
+            {
+                name: "Editor picks",
+                href: "#",
+                imageSrc: "/nav/ui-kits/mixed.jpg",
+            },
+            {
+                name: "new Arrivals",
+                href: "#",
+                imageSrc: "/nav/ui-kits/blue.jpg",
+            },
+            {
+                name: "Bestsellers",
+                href: "#",
+                imageSrc: "/nav/ui-kits/purple.jpg",
+            },
+        ],
+    },
+    {
+        label: "Icons",
+        value: "icons",
+        featured: [
+            {
+                name: "Favorite Icon Picks",
+                href: "#",
+                imageSrc: "/nav/icons/picks.jpg",
+            },
+            {
+                name: "New Arrivals",
+                href: "#",
+                imageSrc: "/nav/icons/new.jpg",
+            },
+            {
+                name: "Bestsellers Icons",
+                href: "#",
+                imageSrc: "/nav/icons/bestsellers.jpg",
+            },
+        ],
+    },
+];
